103e72531027e8bf24a5c6fa05a0cc02f3772c82
中川真人 <masanew01@gmail.com>
2023-05-05 23:38:09 +0900

    時間保存

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,17 +23,52 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
   final String title;
-
   @override
   State<MyHomePage> createState() => AlarmPage();
 }
 
 class AlarmPage extends State<MyHomePage> {
+  List<TimeOfDay> _alarms = [];
+
+  late SharedPreferences _prefs;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadAlarms();
+  }
+
+  Future<void> _loadAlarms() async {
+    _prefs = await SharedPreferences.getInstance();
+    final alarmsJson = _prefs.getStringList('alarms') ?? [];
+    setState(() {
+      _alarms = alarmsJson.map((time) {
+        final parts = time.split(':');
+        return TimeOfDay(
+          hour: int.parse(parts[0]),
+          minute: int.parse(parts[1]),
+        );
+      }).toList();
+    });
+  }
+
+  Future<void> _saveAlarms() async {
+    final alarmsJson = _alarms.map((time) {
+      return '${time.hour}:${time.minute}';
+    }).toList();
+    await _prefs.setStringList('alarms', alarmsJson);
+  }
+
   _timePicker(BuildContext context) async {
     final TimeOfDay? timePicked = await showTimePicker(
-        context: context, initialTime: const TimeOfDay(hour: 6, minute: 0));
+        context: context, initialTime: TimeOfDay(hour: 6, minute: 0));
+    if (timePicked != null) {
+      setState(() {
+        _alarms.add(timePicked);
+      });
+      await _saveAlarms();
+    }
   }
 
   @override
@@ -40,16 +77,33 @@ class AlarmPage extends State<MyHomePage> {
       backgroundColor: Colors.black12,
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: const Text("アラーム"),
+        title: Text("アラーム"),
       ),
-      body: Column(),
+      body: ListView.builder(
+        itemCount: _alarms.length,
+        itemBuilder: (BuildContext context, int index) {
+          final time = _alarms[index];
+          return Card(
+            child: ListTile(
+              title: Text(_formatTime(time)),
+            ),
+          );
+        },
+      ),
       floatingActionButton: FloatingActionButton.large(
         onPressed: () {
           _timePicker(context);
         },
         backgroundColor: Colors.cyan,
-        child: const Icon(Icons.add),
+        child: Icon(Icons.add),
       ),
     );
+  }
+
+  String _formatTime(TimeOfDay time) {
+    final hour = time.hourOfPeriod.toString().padLeft(2, "0");
+    final minute = time.minute.toString().padLeft(2, "0");
+    final period = time.period == DayPeriod.am ? 'AM' : 'PM';
+    return "$hour:$minute $period";
   }
 }
